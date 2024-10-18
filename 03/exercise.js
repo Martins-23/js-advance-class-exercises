5edc18e0af5df05dcb4a7fc7f0c5feb61a3a9611
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2024-10-19 01:14:26 +0200

    Exercise 3 solution

--- 03/exercise.js
+++ 03/exercise.js
@@ -3,7 +3,7 @@ const studentsArray = ['Luis', 'Federico', 'Clàudia', 'Mario', 'Fran', 'Veronic
 // 👀 Edita la variable 'result' para que contenga el segundo elemento de studentsArray.
 // 📝
 
-const result = 0;
+const result = studentsArray[1];
 
 // Do not edit below this line
 module.exports = result;
