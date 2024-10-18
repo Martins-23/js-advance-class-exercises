ebeda6459b72d89c71d6f5a982221946ec02d461
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2024-10-19 01:20:12 +0200

    Additional Exercise 4 solutions

--- 04/exercise.js
+++ 04/exercise.js
@@ -3,7 +3,12 @@ const studentsArray = ['Luis', 'Federico', 'Clàudia', 'Mario', 'Fran', 'Veronic
 // 👀 Edita la variable 'result' para que contenga el último elemento de studentsArray.
 // 📝
 
+/* Option 1 */
 const result = studentsArray[5];
+/* Option 2 */
+// const result = studentsArray[studentsArray.length - 1];
+/* Option 3 */
+// const result = studentsArray.pop();
 
 // Do not edit below this line
 module.exports = result;
