230aad68436cb11179c28eea836c9baaa3b0ae90
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2024-10-19 01:18:28 +0200

    Exercise 19 solution

--- 19/exercise.js
+++ 19/exercise.js
@@ -3,7 +3,7 @@ const studentsArray = ['Luis', 'Federico', 'Clàudia', 'Mario', 'Fran', 'Veronic
 // 👀 Edita la variable 'result' para que contenga la posición de 'Mario' en studentsArray
 // 📝
 
-const result = 0; // Modificar para usar indexOf y devolver la posición de 'Mario'
+const result = studentsArray.indexOf("Mario"); // Modificar para usar indexOf y devolver la posición de 'Mario'
 
 // Do not edit below this line
 module.exports = result;
