5cb549fc9a6d4a5b3dabdc91f7e10cb20482e08c
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2024-10-19 01:18:43 +0200

    Exercise 21 solution

--- 21/exercise.js
+++ 21/exercise.js
@@ -3,7 +3,7 @@ const studentsArray = ['Luis', 'Federico', 'Clàudia', 'Mario', 'Fran', 'Veronic
 // 👀 Edita la variable 'result' para comprobar si todos los elementos tienen más de 3 carácteres
 // 📝
 
-const result = false; // Modificar para usar every y devolver true si todos tienen más de 3 carácteres
+const result = studentsArray.every((student) => student.length > 3); // Modificar para usar every y devolver true si todos tienen más de 3 carácteres
 
 // Do not edit below this line
 module.exports = result;
