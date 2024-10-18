d723e2ef97f1ecf80b57a9e126c11220c9354504
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2024-10-19 01:18:19 +0200

    Exercise 18 solution

--- 18/exercise.js
+++ 18/exercise.js
@@ -3,7 +3,7 @@ const studentsArray = ['Luis', 'Federico', 'Clàudia', 'Mario', 'Fran', 'Veronic
 // 👀 Edita la variable 'result' para que contenga solo los nombres que tengan 5 carácteres o menos
 // 📝
 
-const result = []; // Modificar para usar filter y devolver nombres con 5 carácteres o menos
+const result = studentsArray.filter((student) => student.length <= 5); // Modificar para usar filter y devolver nombres con 5 carácteres o menos
 
 // Do not edit below this line
 module.exports = result;
