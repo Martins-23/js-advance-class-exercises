8d76f92593a79db6cf27e49989825d3646ae46de
Marti Planaguma Sola (PC) <marti.planaguma@icloud.com>
2024-10-19 01:18:51 +0200

    Exercise 22 solution

--- 22/exercise.js
+++ 22/exercise.js
@@ -3,7 +3,7 @@ const studentsArray = ['Luis', 'Federico', 'Clàudia', 'Mario', 'Fran', 'Veronic
 // 👀 Edita la variable 'result' para comprobar si algún elemento del array tiene más de 7 carácteres
 // 📝
 
-const result = false; // Modificar para usar some y devolver true si algún elemento tiene más de 7 carácteres
+const result = studentsArray.some((student) => student.length > 7); // Modificar para usar some y devolver true si algún elemento tiene más de 7 carácteres
 
 // Do not edit below this line
 module.exports = result;
